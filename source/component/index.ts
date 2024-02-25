7fa46364e6ba8aa3a9284d00a4011cfa479d3f57
Ziphil <yfe23457@nifty.com>
2024-02-25 15:13:30 +0900

    変更

--- source/component/index.ts
+++ source/component/index.ts
@@ -12,6 +12,7 @@ export * from "./atom/file-input";
 export * from "./atom/general-icon";
 export * from "./atom/google-adsense";
 export * from "./atom/google-analytics";
+export * from "./atom/icon-button";
 export * from "./atom/input";
 export * from "./atom/link";
 export * from "./atom/loading-icon";
