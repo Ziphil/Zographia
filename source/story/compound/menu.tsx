0a96918278224f1633d90b840db0fbff9c7d596f
Ziphil <yfe23457@nifty.com>
2024-04-06 18:02:07 +0900

    MenuItemIconbagを追加

--- source/story/compound/menu.tsx
+++ source/story/compound/menu.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/jsx-closing-tag-location */
 
+import {faCat, faExclamation} from "@fortawesome/sharp-regular-svg-icons";
 import {action} from "@storybook/addon-actions";
 import {Meta as RawMeta, StoryObj as RawStory} from "@storybook/react";
-import {Menu, MenuItem, MenuSeparator} from "/source/component";
+import {GeneralIcon, Menu, MenuItem, MenuItemIconbag, MenuSeparator} from "/source/component";
 import {createChildren} from "/source/util/children";
 
 
@@ -18,6 +19,32 @@ export default {
 const template = {
 } as Story;
 
+export const withIcon = {
+  ...template,
+  name: "アイコン付き",
+  args: {
+    trigger: <button>表示</button>,
+    triggerType: "click",
+    children: createChildren(<>
+      <MenuItem onClick={action("onItemClick.0")}>
+        <MenuItemIconbag><GeneralIcon icon={faCat}/></MenuItemIconbag>
+        選択肢 1
+      </MenuItem>
+      <MenuItem onClick={action("onItemClick.1")}>
+        <MenuItemIconbag><GeneralIcon icon={faExclamation}/></MenuItemIconbag>
+        選択肢 2
+      </MenuItem>
+      <MenuItem scheme="blue" onClick={action("onItemClick.2")}>
+        <MenuItemIconbag><GeneralIcon icon={faCat}/></MenuItemIconbag>
+        選択肢 3
+      </MenuItem>
+      <MenuItem scheme="red" onClick={action("onItemClick.3")}>
+        <MenuItemIconbag><GeneralIcon icon={faExclamation}/></MenuItemIconbag>
+        選択肢 4
+      </MenuItem>
+    </>)
+  }
+} as Story;
 export const border = {
   ...template,
   name: "ボーダー",
@@ -25,13 +52,13 @@ export const border = {
     trigger: <button>表示</button>,
     triggerType: "click",
     children: createChildren(<>
-      <MenuItem onClick={action("onItemClick.0")}>選択肢1</MenuItem>
-      <MenuItem onClick={action("onItemClick.1")}>選択肢2</MenuItem>
-      <MenuItem onClick={action("onItemClick.2")}>選択肢3</MenuItem>
+      <MenuItem onClick={action("onItemClick.0")}>選択肢 1</MenuItem>
+      <MenuItem onClick={action("onItemClick.1")}>選択肢 2</MenuItem>
+      <MenuItem onClick={action("onItemClick.2")}>選択肢 3</MenuItem>
       <MenuSeparator/>
-      <MenuItem onClick={action("onItemClick.3")}>選択肢4</MenuItem>
-      <MenuItem onClick={action("onItemClick.4")}>選択肢5</MenuItem>
-      <MenuItem onClick={action("onItemClick.5")}>選択肢6</MenuItem>
+      <MenuItem onClick={action("onItemClick.3")}>選択肢 4</MenuItem>
+      <MenuItem onClick={action("onItemClick.4")}>選択肢 5</MenuItem>
+      <MenuItem onClick={action("onItemClick.5")}>選択肢 6</MenuItem>
     </>)
   }
 } as Story;
@@ -43,7 +70,7 @@ export const triggerTypeClick = {
     trigger: <button>表示</button>,
     triggerType: "click",
     children: Array.from({length: 100}).map((dummy, index) => (
-      <MenuItem key={index} onClick={action(`onItemClick.${index}`)}>{`選択肢${index + 1}`}</MenuItem>
+      <MenuItem key={index} onClick={action(`onItemClick.${index}`)}>{`選択肢 ${index + 1}`}</MenuItem>
     ))
   }
 } as Story;
@@ -54,7 +81,7 @@ export const triggerTypeFocus = {
     trigger: <input type="input" value="表示"/>,
     triggerType: "focus",
     children: Array.from({length: 100}).map((dummy, index) => (
-      <MenuItem key={index} onClick={action(`onItemClick.${index}`)}>{`選択肢${index + 1}`}</MenuItem>
+      <MenuItem key={index} onClick={action(`onItemClick.${index}`)}>{`選択肢 ${index + 1}`}</MenuItem>
     ))
   }
 } as Story;
@@ -65,7 +92,7 @@ export const triggerTypeHover = {
     trigger: <div>表示</div>,
     triggerType: "hover",
     children: Array.from({length: 100}).map((dummy, index) => (
-      <MenuItem key={index} onClick={action(`onItemClick.${index}`)}>{`選択肢${index + 1}`}</MenuItem>
+      <MenuItem key={index} onClick={action(`onItemClick.${index}`)}>{`選択肢 ${index + 1}`}</MenuItem>
     ))
   }
 } as Story;
