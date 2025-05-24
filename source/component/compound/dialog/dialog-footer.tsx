f19fa662c048872a48aabd60855098f1057c83d2
Ziphil <yfe23457@nifty.com>
2025-05-24 11:14:30 +0900

    Dialogのフッターを表示できるよう変更

---
+++ source/component/compound/dialog/dialog-footer.tsx
@@ -0,0 +1,26 @@
+//
+
+import {ReactElement, ReactNode, Ref} from "react";
+import {createWithRef} from "/source/component/create";
+import {AdditionalProps} from "/source/module/data";
+
+
+export const DialogFooter = createWithRef(
+  require("./dialog-footer.scss"), "DialogFooter",
+  function ({
+    children,
+    ...rest
+  }: {
+    children: ReactNode,
+    className?: string,
+    ref?: Ref<HTMLDivElement>
+  } & AdditionalProps): ReactElement {
+
+    return (
+      <div styleName="root" {...rest}>
+        {children}
+      </div>
+    );
+
+  }
+);
--- source/component/compound/dialog/index.ts
+++ source/component/compound/dialog/index.ts
@@ -3,6 +3,7 @@
 export * from "./dialog";
 export * from "./dialog-body";
 export * from "./dialog-close-button";
+export * from "./dialog-footer";
 export * from "./dialog-pane";
 export * from "./dialog-outside-button";
 export * from "./dialog-outside-button-container";
